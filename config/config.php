359cd2305bf469fadec6ad7af5a749df171ac1e7
Sri Nikhil Reddy <srinikhilreddy99@gmail.com>
2024-02-14 05:50:32 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@ session_start();
 
 $timezone=date_default_timezone_set("America/New_York");
 
-// $con = mysqli_connect("db", "php_docker", "password", "you.social"); //connection variable
+$con = mysqli_connect("db", "php_docker", "password", "you.social"); //connection variable
 
-$con = mysqli_connect("localhost", "root", "", "you.social"); //connection variable
+// $con = mysqli_connect("localhost", "root", "", "you.social"); //connection variable
 
 
 if (mysqli_connect_errno()) {
